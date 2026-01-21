9b5ec0783bcea5daa7f9c79c678d0f1f606dec6e
danigompal26-ui <danigompal26@gmail.com>
2026-01-22 06:34:00 +0700

    Update database.lua

--- database.lua
+++ database.lua
@@ -1,5 +1,15 @@
 return {
+    [10122791691] = true, -- JDN_1
+    [10122248616] = true, -- JDN_2
+    [10149088398] = true, -- JDN_3
+    [10149157539] = true, -- JDN_4
     [10149187348] = true, -- JDN_6
-    [10149221154] = true, -- JDN_7
+    [10149221154] = true, -- JDN_7    
+    [10174592390] = true, -- JDN_8
+    [10198558498] = true, -- JDN_9
+    [9681798232] = true, -- JustDanny26
+    [9085809657] = true, -- Juple
+    [9845947761] = true, -- Bella
     -- Tambahkan ID lain di sini
+    
 }
